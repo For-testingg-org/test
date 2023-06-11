94da525a75433124e7c229d5b26fb213d158e280
audrey <vaniaaudrey15@gmail.com>
2023-06-11 09:17:59 +0700

    cmt

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Pengaturan server lainnya...
-    headers: {
-      'Permissions-Policy': '', // Menghapus header Permissions-Policy
-    },
-  },
+  // server: {
+  //   // Pengaturan server lainnya...
+  //   headers: {
+  //     'Permissions-Policy': '', // Menghapus header Permissions-Policy
+  //   },
+  // },
 });
 
